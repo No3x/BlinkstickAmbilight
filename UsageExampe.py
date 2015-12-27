c036ba7f7afd6953f3f3747eb8822d0c804c2df8
No3x <noex_home@yahoo.de>
2015-12-27 18:02:19 +0100

    Working version with web simulation.

---
+++ UsageExampe.py
@@ -0,0 +1,18 @@
+from Ambilight import Ambilight
+from VirtualBlinkstick import VirtualBlinkStick
+import math
+import time
+
+ambilight = Ambilight(200, 50)
+blinkstick = VirtualBlinkStick()
+blinkstick.connect()
+
+while True:
+    try:
+        ambilight.run()
+        colors = ambilight.currentColors.items()
+        blinkstick.setCurrentColors( colors )
+        blinkstick.run();
+    except KeyboardInterrupt:
+        print 'Interrupted'
+
--- ambilight.py
+++ ambilight.py
@@ -1,26 +1,30 @@
+import collections
+
 __author__ = 'Christian'
 import pyscreeze
 import Tkinter
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageStat
 
 root = Tkinter.Tk()
 
-
 class Ambilight:
-    def __init__(self):
-        self.NUMLED = 60
-        self.BORDER = 50
+    def __init__(self, led_num, border):
+        self.NUMLED = led_num
+        self.BORDER = border
         self.WIDTH = root.winfo_screenwidth()
         self.HEIGHT = root.winfo_screenheight()
+        self.STRIPE_LENGTH = (2 * self.HEIGHT) + (2 * self.WIDTH)
+        self.currentColors = collections.OrderedDict()
 
     def run(self):
-        top, right, bottom, left = self.makeImages()
+        top, right, bottom, left = self.__makeImages()
         # self.doMath(top)
-        #self.save(top)
-        self.concat(top, right, bottom, left)
-        self.drawBalkens()
+        # self.__save(right, "right_debug.png")
+        self.__concat(top, right, bottom, left)
+        #self.__drawBalkens()
+        self.__calcColors()
 
-    def makeImages(self):
+    def __makeImages(self):
         # start_x, start_y, breite, hoehe
         top = pyscreeze.screenshot(region=(0, 0, self.WIDTH, self.BORDER), imageFilename='top.png')
         right = pyscreeze.screenshot(region=(self.WIDTH - self.BORDER, 0, self.BORDER, self.HEIGHT),
@@ -31,17 +35,17 @@ class Ambilight:
         return top, right, bottom, left
 
 
-    def doMath(self, im):
+    def __doMath(self, im):
         draw = ImageDraw.Draw(im)
         draw.line((0, 0) + im.size, fill=128)
         draw.line((0, im.size[1], im.size[0], 0), fill=128)
         del draw
 
-    def save(self, im):
+    def __save(self, im, name):
         # write
-        im.save('top_after.png', "PNG")
+        im.save(name, "PNG")
 
-    def concat(self, top, right, bottom, left):
+    def __concat(self, top, right, bottom, left):
         right = right.rotate(90)
         bottom = bottom.rotate(-180)
         left = left.rotate(-90)
@@ -56,7 +60,7 @@ class Ambilight:
         print top.size[0] + right.size[0] + bottom.size[0]
         blank_image.save('all.png')
 
-    def drawBalkens(self):
+    def __drawBalkens(self):
         im = Image.open('all.png')
         draw = ImageDraw.Draw(im)
         width, height = im.size
@@ -70,5 +74,58 @@ class Ambilight:
         del draw
         im.save('all.png')
 
+    def __improvedMedian(self, im):
+        # grab width and height
+        width, height = im.size
 
-Ambilight().run()
+        # make a list of all pixels in the image
+        pixels = im.load()
+        data = []
+        for x in range(width):
+            for y in range(height):
+                cpixel = pixels[x, y]
+                data.append(cpixel)
+
+        r = 0
+        g = 0
+        b = 0
+        counter = 0
+
+        # loop through all pixels
+        # if alpha value is greater than 200/255, add it to the average
+        # (note: could also use criteria like, if not a black pixel or not a white pixel...)
+        for x in range(len(data)):
+            try:
+                if data[x][3] > 200:
+                    r+=data[x][0]
+                    g+=data[x][1]
+                    b+=data[x][2]
+            except:
+                r+=data[x][0]
+                g+=data[x][1]
+                b+=data[x][2]
+
+            counter+=1
+
+        # compute average RGB values
+        rAvg = r/counter
+        gAvg = g/counter
+        bAvg = b/counter
+
+        return [rAvg, gAvg, bAvg]
+
+    def __calcColors(self):
+        all = Image.open('all.png')
+        i = 0
+        count = 0
+        width, height = all.size
+        div = width / self.NUMLED
+        while (i <= self.STRIPE_LENGTH):
+            # skip first chunk
+            if i >= self.STRIPE_LENGTH / self.NUMLED:
+                count = count + 1
+                chunk = all.crop(box=(count*div, 0, self.STRIPE_LENGTH - (self.NUMLED-count)*div, self.BORDER))
+                #hex = self.__improvedMedian(chunk)#ImageStat.Stat(chunk)._getmean()
+                self.currentColors[count] = ImageStat.Stat(chunk)._getmean()
+            i = i + self.STRIPE_LENGTH / self.NUMLED
+        assert count == self.NUMLED
