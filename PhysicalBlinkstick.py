88fb24ce88ba2a3c6b2d73e1856c4ad111695c14
No3x <noex_home@yahoo.de>
2016-01-19 21:29:47 +0100

    Working version. Prompt, improved image concat, improved image color calc

---
+++ PhysicalBlinkstick.py
@@ -0,0 +1,74 @@
+import collections
+
+__author__ = 'Christian'
+from blinkstick import blinkstick
+from Ambilight import Ambilight
+from Prompt import Prompt
+import time
+import math
+from random import randint
+NUMLED = 4
+BORDER = 50
+
+class Blinkstick2812(blinkstick.BlinkStickPro):
+
+    def __init__(self, ambilight=None, r_led_count=None, max_rgb_value=None, delay=None):
+        #self.bstick = blinkstick.BlinkStickPro.__init__(self, r_led_count=r_led_count, max_rgb_value=max_rgb_value, delay=delay )
+        self.ambilight = ambilight
+        blinkstick.BlinkStickPro.__init__(self, r_led_count=r_led_count, max_rgb_value=max_rgb_value, delay=delay )
+
+    def insureWSMode(self):
+        mode = self.bstick.get_mode()
+        if mode is not 2:
+            if Prompt().prompt.query_yes_no("Should I set the proper mode for you now? (Mode 2 : WS2812)", "no"):
+                print "Set mode to 2"
+                self.bstick.set_mode(2)
+                print "Please reconnect your BlinkStick"
+            else:
+                raise ValueError('BlinkStick is not in proper mode (see https://www.blinkstick.com/help/tutorials/blinkstick-pro-modes). Exit')
+
+    # Round values
+    def __round(self, colors):
+        roundedColors = []
+        for k, v in colors:
+            ( R, G, B ) = v
+            R = math.floor(R)
+            G = math.floor(G)
+            B = math.floor(B)
+            v = (R, G, B)
+            roundedColors.append(v)
+        return roundedColors
+
+    def run(self):
+        try:
+            while 1:
+                self.send_data_all()
+                self.ambilight.run()
+                #
+                # colors = collections.OrderedDict()
+                # colors[0] = (161, 174, 188)
+                # colors[1] = (161, 174, 188)
+                # colors[2] = (161, 174, 188)
+                # colors[3] = (161, 174, 188)
+                colors = ambilight.currentColors
+                for k, v in colors.items():
+                    #print k, v
+                    (R, G, B) = v
+                    R = int( math.floor( R ) )
+                    G = int( math.floor( G ) )
+                    B = int( math.floor( B ) )
+                    self.set_color(0, k, R, G, B)
+                time.sleep(0.01)
+
+        except KeyboardInterrupt:
+            self.off()
+            return
+
+
+ambilight=Ambilight(NUMLED, BORDER)
+blinkstick2812 = Blinkstick2812(ambilight=ambilight, r_led_count=NUMLED, max_rgb_value=255, delay=0.002)
+if blinkstick2812.connect():
+    blinkstick2812.insureWSMode()
+    blinkstick2812.run()
+else:
+    print "No BlinkSticks found"
--- ambilight.py
+++ ambilight.py
@@ -18,7 +18,6 @@ class Ambilight:
 
     def run(self):
         top, right, bottom, left = self.__makeImages()
-        # self.__save(right, "right_debug.png")
         self.__concat(top, right, bottom, left)
         self.__drawBalkens()
         self.__calcColors()
@@ -29,8 +28,14 @@ class Ambilight:
         right = pyscreeze.screenshot(region=(self.WIDTH - self.BORDER, 0, self.BORDER, self.HEIGHT))
         bottom = pyscreeze.screenshot(region=(0, self.HEIGHT - self.BORDER, self.WIDTH, self.BORDER))
         left = pyscreeze.screenshot(region=(0, 0, self.BORDER, self.HEIGHT))
-        return top, right, bottom, left
 
+        # Loop over ImageCrop to make Images
+        out = []
+        for i, image in enumerate( (top, right, bottom, left) ):
+            image_copy = Image.new(image.mode, image.size)
+            image_copy.putdata(list(image.getdata()))
+            out.append( (image_copy) )
+        return out[0], out[1], out[2], out[3]
 
     def __doMath(self, im):
         draw = ImageDraw.Draw(im)
@@ -42,18 +47,16 @@ class Ambilight:
         im.save(name, "PNG")
 
     def __concat(self, top, right, bottom, left):
-        right = right.rotate(90)
-        bottom = bottom.rotate(-180)
-        left = left.rotate(-90)
-        # top.size[0] = width, top.size[1] = height
-        blank_image = Image.new("RGB", (top.size[0] + right.size[0] + bottom.size[0] + left.size[0], top.size[1]))
+        rotated_images = [(image.rotate(90, expand=True) if image.height > image.width else image) for image in [top, right, bottom, left]]
+        width_total = sum(int(v.size[0]) for v in rotated_images)
+        blank_image = Image.new("RGB", (width_total, min(top.size)))
         # Image.paste(im, ( x, y) )
-        blank_image.paste(top, (0, 0))
-        blank_image.paste(right, (top.size[0], 0))
-        blank_image.paste(bottom, (top.size[0] + right.size[0], 0))
-        blank_image.paste(left, (top.size[0] + right.size[0] + bottom.size[0], 0))
-        # print top.size[0] + right.size[0] + bottom.size[0]
+        width_processed = 0
+        for image in rotated_images:
+            blank_image.paste( image, ( width_processed, 0) )
+            width_processed += image.size[0]
         self.current_image_all = blank_image
+        #self.__save(self.current_image_all, 'image_after_concat.png')
 
     def __drawBalkens(self):
         draw = ImageDraw.Draw(self.current_image_all)
@@ -107,15 +110,20 @@ class Ambilight:
 
     def __calcColors(self):
         i = 0
-        count = 0
+        chunks_done = 0
         width, height = self.current_image_all.size
         div = width / self.NUMLED
-        while (i <= self.STRIPE_LENGTH):
-            # skip first chunk
-            if i >= self.STRIPE_LENGTH / self.NUMLED:
-                count = count + 1
-                chunk = self.current_image_all.crop(box=(count*div, 0, self.STRIPE_LENGTH - (self.NUMLED-count)*div, self.BORDER))
-                #hex = self.__improvedMedian(chunk)#ImageStat.Stat(chunk)._getmean()
-                self.currentColors[count] = ImageStat.Stat(chunk)._getmean()
-            i = i + self.STRIPE_LENGTH / self.NUMLED
-        assert count == self.NUMLED
+        while (i <= self.STRIPE_LENGTH-1*div):
+            # left, upper, right, and lower pixel
+            left = i
+            right = left+div;
+            if( self.NUMLED == chunks_done-1 ):
+                right = self.STRIPE_LENGTH
+            lower = self.BORDER
+            upper = 0
+            chunk = self.current_image_all.crop(box=(left, upper, right, lower ) )
+            #hex = self.__improvedMedian(chunk)#ImageStat.Stat(chunk)._getmean()
+            self.currentColors[chunks_done] = ImageStat.Stat(chunk)._getmean()
+            chunks_done = chunks_done + 1
+            i = i + div
+        assert chunks_done == self.NUMLED
